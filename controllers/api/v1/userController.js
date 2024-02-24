909f2dfc23bee8a128152adf1c9599ff32bdaefd
Yannick Van den Brande <yavadebr@gmail.com>
2024-02-24 17:25:57 +0100

    feat: get user by ID

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -13,6 +13,35 @@ const getAllUsers = async (req, res) => {
     }
 };
 
+// get user by ID
+const getUserById = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        // check if ID is valid
+        if (!userId) {
+            return res.status(400).json({ message: 'User ID is required' });
+        }
+
+        // fetch user by ID
+        const user = await User.findById(userId);
+
+        // check if user with provided ID exists
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User fetched successfully', data: { user } });
+    } catch (error) {
+        if (error.name === 'CastError') {
+            return res.status(400).json({ message: 'Invalid user ID format' });
+        }
+
+        console.error('Error fetching user by ID:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 // create new user
 const createUser = async (req, res) => {
     try {
@@ -45,5 +74,6 @@ const createUser = async (req, res) => {
 
 module.exports = {
     getAllUsers,
+    getUserById,
     createUser
 };
--- routes/api/v1/user.js
+++ routes/api/v1/user.js
@@ -5,6 +5,9 @@ const userController = require('../../../controllers/api/v1/userController');
 // GET all users
 router.get('/', userController.getAllUsers);
 
+// GET user by ID
+router.get('/:id', userController.getUserById);
+
 // POST new user
 router.post('/', userController.createUser);
 
