8639ccdcafd0f88f869f4fed16e4ae7c64e39b6e
Yannick Van den Brande <yavadebr@gmail.com>
2024-02-24 16:15:30 +0100

    feat: check if company exists

--- controllers/api/v1/businessController.js
+++ controllers/api/v1/businessController.js
@@ -3,10 +3,16 @@ const Business = require('../../../models/api/v1/Business');
 // create new business
 const createBusiness = async (req, res) => {
     try {
-        // create new business instance
+        // check if the company name already exists
+        const existingBusiness = await Business.findOne({ name: req.body.name });
+        if (existingBusiness) {
+            return res.status(400).json({ message: 'Company name already exists' });
+        }
+
+        // create a new business instance
         const newBusiness = new Business(req.body);
 
-        // save new business to database
+        // save the new business to the database
         await newBusiness.save();
 
         res.status(201).json({ message: 'Business created successfully', data: { business: newBusiness } });
