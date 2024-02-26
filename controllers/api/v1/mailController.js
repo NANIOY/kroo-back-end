3eb74ab70cb14eeda11eb05fe7c06c9cb3ac38a4
Yannick Van den Brande <yavadebr@gmail.com>
2024-02-26 19:59:27 +0100

    feat: add random invitation code to mail

--- controllers/api/v1/mailController.js
+++ controllers/api/v1/mailController.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const { v4: uuidv4 } = require('uuid');
 const Business = require('../../../models/api/v1/Business');
 
 const emailUser = process.env.EMAIL_USER;
@@ -12,22 +13,27 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+const generateRandomCode = () => {
+    const uuid = uuidv4(); // Generate a UUID
+    const truncatedCode = uuid.replace(/-/g, '').slice(0, 8); // remove hyphens and take first 8 characters
+    return truncatedCode;
+};
+
 // function to send email to invited employees
 const sendInvite = async (req, res) => {
     try {
         const businessId = req.params.id;
         const employees = req.body;
+        
+        console.log(`Sending invites for business ID: ${businessId}`);
 
-        // retrieve business name based on businessId
+        // fetch business information
         const business = await Business.findById(businessId);
         if (!business) {
             return res.status(404).json({ message: 'Business not found' });
         }
-        const businessName = business.name;
 
-        console.log(`Sending invites for business ID: ${businessId}`);
-
-        await sendEmailToEmployees(employees, businessName);
+        await sendEmailToEmployees(employees, business);
         res.status(200).json({ message: 'Emails sent successfully' });
     } catch (error) {
         console.error('Error sending emails:', error);
@@ -36,15 +42,16 @@ const sendInvite = async (req, res) => {
 };
 
 // function to send email to invited employees when business is created
-const sendEmailToEmployees = async (employees, businessName) => {
+const sendEmailToEmployees = async (employees, business) => {
     try {
         // send email to each employee
         for (const employee of employees) {
-            const emailContent = `You have been invited to ${businessName} as ${employee.role}`;
+            const randomCode = generateRandomCode();
+            const emailContent = `You have been invited to ${business.name} as ${employee.role}. Your invitation code is: ${randomCode}`;
             await transporter.sendMail({
                 from: 'hello@kroo.site',
                 to: employee.email,
-                subject: 'Invitation to Join Business',
+                subject: 'Invitation to the Business',
                 text: emailContent
             });
         }
