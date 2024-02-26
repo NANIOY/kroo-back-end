5c54168031ee91081e2ce88d86c9bbee3e45120a
Yannick Van den Brande <yavadebr@gmail.com>
2024-02-26 19:53:46 +0100

    feat: dynamically add business name + role to mail

--- controllers/api/v1/mailController.js
+++ controllers/api/v1/mailController.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const Business = require('../../../models/api/v1/Business');
 
 const emailUser = process.env.EMAIL_USER;
 const emailPassword = process.env.EMAIL_PASSWORD;
@@ -16,10 +17,17 @@ const sendInvite = async (req, res) => {
     try {
         const businessId = req.params.id;
         const employees = req.body;
-        
+
+        // retrieve business name based on businessId
+        const business = await Business.findById(businessId);
+        if (!business) {
+            return res.status(404).json({ message: 'Business not found' });
+        }
+        const businessName = business.name;
+
         console.log(`Sending invites for business ID: ${businessId}`);
 
-        await sendEmailToEmployees(employees);
+        await sendEmailToEmployees(employees, businessName);
         res.status(200).json({ message: 'Emails sent successfully' });
     } catch (error) {
         console.error('Error sending emails:', error);
@@ -28,15 +36,16 @@ const sendInvite = async (req, res) => {
 };
 
 // function to send email to invited employees when business is created
-const sendEmailToEmployees = async (employees) => {
+const sendEmailToEmployees = async (employees, businessName) => {
     try {
         // send email to each employee
         for (const employee of employees) {
+            const emailContent = `You have been invited to ${businessName} as ${employee.role}`;
             await transporter.sendMail({
                 from: 'hello@kroo.site',
                 to: employee.email,
-                subject: 'Subject of the email',
-                text: 'Body of the email'
+                subject: 'Invitation to Join Business',
+                text: emailContent
             });
         }
         console.log('Emails sent successfully');
