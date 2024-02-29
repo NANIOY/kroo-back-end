984f91b1931a9ba0d6bd7a5f8e8a80352f15c00c
Nanio <111894274+NanioY@users.noreply.github.com>
2024-02-29 15:45:47 +0100

    feat: send mail through Postmark

--- controllers/api/v1/mailController.js
+++ controllers/api/v1/mailController.js
@@ -1,17 +1,29 @@
-const nodemailer = require('nodemailer');
+const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 const Business = require('../../../models/api/v1/Business');
 
-const emailUser = process.env.EMAIL_USER;
-const emailPassword = process.env.EMAIL_PASSWORD;
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+const sendEmail = async (to, subject, text) => {
+    try {
+        const response = await axios.post('https://api.postmarkapp.com/email', {
+            From: 'hello@kroo.site',
+            To: to,
+            Subject: subject,
+            TextBody: text,
+            MessageStream: 'outbound',
+        }, {
+            headers: {
+                'X-Postmark-Server-Token': process.env.POSTMARK_SERVER_TOKEN,
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log('Email sent successfully:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error sending email:', error.response.data);
+        throw error;
     }
-});
+};
+
 
 const generateRandomCode = () => {
     const uuid = uuidv4(); // Generate a UUID
@@ -19,41 +31,14 @@ const generateRandomCode = () => {
     return truncatedCode;
 };
 
-// function to send email to invited employees
-const sendInvite = async (req, res) => {
-    try {
-        const businessId = req.params.id;
-        const employees = req.body;
-        
-        console.log(`Sending invites for business ID: ${businessId}`);
-
-        // fetch business information
-        const business = await Business.findById(businessId);
-        if (!business) {
-            return res.status(404).json({ message: 'Business not found' });
-        }
-
-        await sendEmailToEmployees(employees, business);
-        res.status(200).json({ message: 'Emails sent successfully' });
-    } catch (error) {
-        console.error('Error sending emails:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-};
-
-// function to send email to invited employees when business is created
 const sendEmailToEmployees = async (employees, business) => {
     try {
         // send email to each employee
         for (const employee of employees) {
             const randomCode = generateRandomCode();
             const emailContent = `You have been invited to ${business.name} as ${employee.role}. Your invitation code is: ${randomCode}`;
-            await transporter.sendMail({
-                from: 'hello@kroo.site',
-                to: employee.email,
-                subject: 'Invitation to the Business',
-                text: emailContent
-            });
+            await sendEmail(employee.email, 'Invitation to the Business', emailContent);
+            console.log(`Email sent to ${employee.email}`);
         }
         console.log('Emails sent successfully');
     } catch (error) {
@@ -62,7 +47,29 @@ const sendEmailToEmployees = async (employees, business) => {
     }
 };
 
+const sendInvite = async (req, res) => {
+    try {
+        const businessId = req.params.id; // extracting business ID from URL params
+        console.log(`Sending invites for business ID: ${businessId}`);
+
+        // fetch business information
+        const business = await Business.findById(businessId);
+        if (!business) {
+            return res.status(404).json({ message: 'Business not found' });
+        }
+
+        // extract email from request body
+        const { email } = req.body;
+
+        await sendEmailToEmployees([{ email }], business);
+        res.status(200).json({ message: 'Emails sent successfully' });
+    } catch (error) {
+        console.error('Error sending emails:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 module.exports = {
-    sendInvite,
-    sendEmailToEmployees
+    sendEmailToEmployees,
+    sendInvite
 };
