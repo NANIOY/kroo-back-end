40e049638d9b1d22d6db169232e44fe2b2acd20b
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-07 20:12:21 +0100

    feat: add route for password reset

--- routes/api/v1/mail.js
+++ routes/api/v1/mail.js
@@ -5,4 +5,7 @@ const mailController = require('../../../controllers/api/v1/mailController');
 // POST email to invited employees
 router.post('/:id/invite', mailController.sendInvite);
 
+// POST request for password reset
+router.post('/passwordreset', mailController.sendPasswordResetEmail);
+
 module.exports = router;
