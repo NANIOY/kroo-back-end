a34462c89f0fe7e1f2d694121ea63933dab42e85
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-08 19:31:56 +0100

    docs: remove rate limit comment

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../../../controllers/api/v1/authController');
 
-// handle user login (rate limited)
+// handle user login
 router.post('/login', authController.login);
 
 module.exports = router;
