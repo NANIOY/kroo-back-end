7ad59683321a71293bb6b3e7299566d967fc9c15
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-19 17:58:16 +0100

    feat: prepare teamController

---
+++ controllers/api/v1/business/teamController.js
@@ -0,0 +1,8 @@
+const { User } = require('../../../../models/api/v1/User');
+const Business = require('../../../../models/api/v1/Business');
+const { CustomError } = require('../../../../middlewares/errorHandler');
+
+
+modules.export = {
+    
+}
