56e22972b6c43ac94375bbe4e00db0942d28fdac
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-24 14:54:44 +0200

    feat: upload file to cloudinary

--- controllers/api/v1/shared/fileController.js
+++ controllers/api/v1/shared/fileController.js
@@ -1,25 +1,23 @@
-const File = require('../../../../models/api/v1/File');
+const cloudinary = require('cloudinary').v2;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-const uploadFile = async (req, res) => {
+const upload = async (req, res) => {
   try {
-    const { originalname, filename, mimetype, size } = req.file;
+    // upload file to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, { folder: 'user-images' });
 
-    const file = new File({
-      originalName: originalname,
-      filename: filename,
-      mimetype: mimetype,
-      size: size,
-    });
-    await file.save();
-
-    res.status(200).json({ message: 'File uploaded successfully', fileId: file._id });
+    // if file is uploaded, return URL
+    res.json(result);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    // return error if file is not uploaded
+    console.error('Error uploading image:', error);
+    res.status(500).json({ error: 'Error uploading image' });
   }
 };
 
-module.exports = {
-  uploadFile,
-};
+module.exports = { upload };
--- routes/api/v1/file.js
+++ routes/api/v1/file.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const multer = require('multer');
-const { uploadFile } = require('../../../controllers/api/v1/shared/fileController');
+const fileController = require('../../../controllers/api/v1/shared/fileController');
 
+const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/upload', upload.single('file'), uploadFile);
+router.post('/upload', upload.single('file'), fileController.upload);
 
 module.exports = router;
