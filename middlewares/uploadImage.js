02204b1f64c012c1be4fa6155b3a2a85681441e5
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-24 16:00:48 +0200

    chore: remove uploadimage middleware

--- middlewares/uploadImage.js
+++
@@ -1,8 +0,0 @@
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const uploadImage = upload.single('image');
-
-module.exports = {
-    uploadImage
-};
--- routes/api/v1/crew.js
+++ routes/api/v1/crew.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const crewController = require('../../../controllers/api/v1/crew/crewController');
-const { uploadImage } = require('../../../middlewares/uploadImage');
 
 // GET all crew data
 router.get('/', crewController.getAllCrewData);
@@ -18,8 +17,5 @@ router.patch('/', crewController.updateCrewData);
 
 // DELETE crew data by ID
 router.delete('/:id', crewController.deleteCrewData);
-
-// POST route for handling image uploads
-router.post('/upload-image', uploadImage, crewController.handleImageUpload);
 
 module.exports = router;
